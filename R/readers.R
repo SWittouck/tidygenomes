e1de37f8645f7222db78027c4b844776ec34abeb
Stijn Wittouck <stijn.wittouck@hotmail.com>
2018-12-17 11:43:07 +0100

    Correct bug in read_phylip_distmat function

--- R/readers.R
+++ R/readers.R
@@ -6,24 +6,27 @@
 # of lines before the actual distance values start.
 read_phylip_distmat <- function(fin, include_diagonal = T, skip = 8) {
 
-  distances_raw <- read_tsv(fin, col_names = F, skip = skip) %>%
+  distances_raw <- 
+    read_tsv(fin, col_names = F, skip = skip) %>%
     separate(ncol(.), into = c("name", "number"), sep = " ")
   
   names <- distances_raw$name
   
   n <- length(names)
   
-  distances <- distances_raw %>%
-    select(2:(n + 1)) %>%
+  distances <- 
+    distances_raw %>%
+    select_if(~ ! all(is.na(.))) %>%
+    select(1:(!! n)) %>%
     `names<-`(names) %>%
     mutate(sequence_1 = !! names) %>%
     gather(key = "sequence_2", value = "distance", - sequence_1, na.rm = T) %>%
     mutate_at("distance", as.double)
   
-  distances_1 <- distances %>%
-    filter(sequence_1 > sequence_2)
+  distances_1 <- filter(distances, sequence_1 > sequence_2)
   
-  distances_2 <- distances %>%
+  distances_2 <- 
+    distances %>%
     filter(sequence_1 < sequence_2) %>%
     mutate(sequence_1_temp = sequence_2, sequence_2_temp = sequence_1) %>%
     select(sequence_1 = sequence_1_temp, sequence_2 = sequence_2_temp, distance)
@@ -31,8 +34,7 @@ read_phylip_distmat <- function(fin, include_diagonal = T, skip = 8) {
   distances <- bind_rows(distances_1, distances_2)
   
   if (! include_diagonal) {
-    distances <- distances %>%
-      filter(sequence_1 != sequence_2)
+    distances <- filter(distances, sequence_1 != sequence_2)
   }
   
   distances

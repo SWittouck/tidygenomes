7848c6bdc36d100e4922cf43552b1fc453dd3c30
Stijn Wittouck <stijn.wittouck@hotmail.com>
2019-03-21 15:40:31 +0100

    Write function upset_plot and write and change other functions for that

--- R/handlers.R
+++ R/handlers.R
@@ -1,6 +1,21 @@
 filter_orthogroups <- function(tog, ...) {
   
   tog %>%
-    modify_at("orthogroups", filter, ...)
+    modify_at("orthogroups", filter, ...) %>%
+    modify_at("genes", filter, orthogroup %in% .$orthogroups$orthogroup)
+  
+}
+
+filter_patterns <- function(tp, ...) {
+  
+  tp %>%
+    modify_at("patterns", filter, ...) %>%
+    modify_at("components", filter, pattern %in% .$patterns$pattern)
+  
+}
+
+mutate_genomes <- function(tog, ...) {
+  
+  modify_at(tog, "genomes", mutate, ...)
   
 }
--- R/plotters.R
+++ R/plotters.R
@@ -51,3 +51,68 @@ plot_genomes_per_species <- function(genomes, metric1, metric2, cladevar = clade
   }
   
 }
+
+upset_plot <- function(tog, genome_name, genome_col, genome_bold, n = 50) {
+    
+  genome_name <- rlang::enexpr(genome_name) 
+  genome_col <- rlang::enexpr(genome_col)
+  genome_bold <- rlang::enexpr(genome_bold)
+  
+  tidypatterns <- 
+    tidypatterns(tog) %>%
+    modify_at("patterns", arrange, desc(n)) %>%
+    modify_at("patterns", mutate, rank = 1:n()) %>%
+    filter_patterns(rank <= !! n) %>%
+    modify_at("patterns", mutate, pattern_ordered = factor(pattern, levels = pattern))
+  
+  theme_upset <- 
+    theme(
+      axis.text.x = element_blank(), 
+      axis.title = element_blank(),
+      axis.ticks = element_blank(), 
+      panel.background = element_rect(fill = "transparent"), 
+      plot.background = element_rect(fill = "transparent")
+  )
+  
+  tog$genomes <-
+    tog$genomes %>%
+    arrange(!! genome_name) %>%
+    group_by(!! genome_col) %>%
+    nest() %>%
+    mutate(genome_col = rep_len(as.character(1:12), nrow(.))) %>%
+    unnest() %>%
+    ungroup()
+  
+  tog$genomes <- rename(tog$genomes, genome_bold = !! genome_bold)
+  
+  plot_main <- 
+    tidypatterns$components %>%
+    left_join(tidypatterns$patterns) %>%
+    left_join(tog$genomes) %>%
+    ggplot(aes(x = pattern_ordered, y = !! genome_name, group = pattern_ordered)) +
+    geom_line(col = "grey") +
+    geom_point(size = 3, aes(col = genome_col)) +
+    scale_color_brewer(palette = "Paired", guide = "none") +
+    theme_bw() +
+    theme_upset +
+    theme(axis.text.y = element_text(face = if_else(tog$genomes$genome_bold, "bold", "plain")))
+
+  plot_marg <- 
+    tidypatterns$patterns %>% 
+    ggplot(aes(x = pattern_ordered, y = n)) +
+    geom_histogram(stat = "identity") +
+    # geom_text(aes(label = n), vjust= - 0.25, size = 2) + 
+    ylim(c(0, 1.1 * max(tidypatterns$patterns$n))) +
+    theme_bw() +
+    theme_upset +
+    theme(
+      panel.border = element_blank(),
+      panel.grid = element_blank()
+    )
+  
+  egg::ggarrange(
+    plot_marg, plot_main,
+    nrow = 2, ncol = 1, heights = c(1, nrow(lgc$genomes) / 50)
+  )
+  
+}

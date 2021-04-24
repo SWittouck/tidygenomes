SWittouck/tidygenomes

commit 5610cbca0fa74c729cf652de47a345e5846f9f2f

Allow for existing occurrence table in add_patterns

R/plotters.R
@@ -92,8 +92,9 @@ upset_plot <- function(
     mutate(genome_name_fct = factor(
       !! genome_name, levels = !! genome_name
     )) %>%
-    mutate(genome_bold = !! genome_bold) %>%
-    select(- is_phylogroup_ancestor, - node) 
+    mutate(genome_bold = !! genome_bold) 
+    # # the following line gives trouble and seems unnecessary
+    # select(- is_phylogroup_ancestor, - node) 
   
   if (phylogroups) {
     

R/enhancers.R
@@ -28,12 +28,21 @@
 #' @export
 add_patterns <- function(tg) {
   
-  if (is.null(tg$genes)) stop("No gene table present")
+  if (is.null(tg$genes) & is.null(tg$occurrences)) {
+    stop("No gene or occurrence table present")
+  } 
   if (is.null(tg$orthogroups)) stop("No orthogroup table present")
   
+  # if an occurrence table has been manually defined, it should not be
+  # recalculated
+  if ("occurrences" %in% names(tg)) {
+    occurrences <- tg$occurrences %>% select(genome, orthogroup)
+  } else {
+    occurrences <- tg$genes %>% distinct(genome, orthogroup) 
+  }
+  
   patterns_raw <-
-    tg$genes %>%
-    distinct(genome, orthogroup) %>%
+    occurrences %>%
     mutate(present = TRUE) %>%
     spread(key = genome, value = present, fill = FALSE) %>%
     nest(orthogroup) %>%

commit 8dc13191cc9904dd4312e14b15fa1729b44ab7ef

Fix bug introduced by commit 5610cbc

R/plotters.R
@@ -92,9 +92,10 @@ upset_plot <- function(
     mutate(genome_name_fct = factor(
       !! genome_name, levels = !! genome_name
     )) %>%
-    mutate(genome_bold = !! genome_bold) 
-    # # the following line gives trouble and seems unnecessary
-    # select(- is_phylogroup_ancestor, - node) 
+    mutate(genome_bold = !! genome_bold)
+  
+  genomes$is_phylogroup_ancestor <- NULL
+  genomes$node <- NULL
   
   if (phylogroups) {
     